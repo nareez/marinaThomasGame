59a18ff5cf93bc9b665b09c8ecc2254fc45574a2
Luiz Gustavo <lugu.almeida@gmail.com>
2021-12-27 01:11:22 -0300

    better jump and animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,44 +4,51 @@ onready var ground_check = $ground_check
 onready var anim = $anim
 var velocity = Vector2.ZERO
 var move_speed = 240
+const MAX_MOVESPEED = 40000
 var gravity = 20
-var jump_force = -500
+var jump_force = -750
 var is_grounded = true
 enum {RUN, IDLE, GOING_UP, FALL}
 var animState = RUN
 var is_do_jump = false
+var is_going_down = false
 
 func _physics_process(delta):
 	# Gravity and running
 	velocity.y += gravity
 	velocity.x = move_speed
-	if is_do_jump:
-		velocity.y = jump_force
-		is_do_jump = false
 	velocity = move_and_slide(velocity)
 
 func _process(delta):
-	_check_state()
-	pass
+	_process_state()
 
 func _input(event):
 	if event.is_action_pressed("jump") and is_grounded:
-		ground_check.set_enabled(false)
-		anim.play("jump")
-		anim.queue("fall")
-		yield(get_tree().create_timer(.4), "timeout")
-		ground_check.set_enabled(true)
-		
-		
+		is_do_jump = true
 
-func jump():
-	is_do_jump = true
-
-func _check_state():
+func _process_state():
 	if ground_check.is_colliding():
 		is_grounded = true
 		anim.play("run")
 	else:
 		is_grounded = false
-	
+		
+	if is_do_jump and is_grounded:
+		ground_check.set_enabled(false)
+		is_do_jump = false
+		velocity.y = jump_force
+		anim.play("jump")
+		anim.queue("going-up")
+		yield(get_tree().create_timer(.3), "timeout")
+		ground_check.set_enabled(true)
+		
+	if velocity.y > 0 and not is_grounded:
+		anim.play("fall")
 
+
+
+
+func _on_Timer_timeout():
+	move_speed *= 1.3
+	if move_speed > MAX_MOVESPEED:
+		move_speed = MAX_MOVESPEED
