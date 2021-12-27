56db5d76882e1c02c5f7451fffc710104bac7126
Luiz Gustavo <lugu.almeida@gmail.com>
2021-12-27 12:10:49 -0300

    parallax background and player animation speedup

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,11 +5,10 @@ onready var anim = $anim
 var velocity = Vector2.ZERO
 var move_speed = 240
 const MAX_MOVESPEED = 4000
+var move_speed_multiplier = 1.2
 var gravity = 20
 var jump_force = -750
 var is_grounded = true
-enum {RUN, IDLE, GOING_UP, FALL}
-var animState = RUN
 var is_do_jump = false
 var is_going_down = false
 
@@ -48,8 +47,15 @@ func _process_state():
 	if velocity.y > 0 and not is_grounded:
 		anim.play("fall")
 
-
+# A cada passada de tempo aumenta a velocidade do personagem
 func _on_Timer_timeout():
-	move_speed *= 1.3
+	var runAnim = anim.get_animation("run")
+	var idx = runAnim.find_track("anim:playback_speed")
+	var animSpeed = runAnim.track_get_key_value(idx, 0)
+	
+	move_speed *= move_speed_multiplier
+	animSpeed *= move_speed_multiplier / 1.1
+	runAnim.track_set_key_value(idx, 0, animSpeed)
 	if move_speed > MAX_MOVESPEED:
 		move_speed = MAX_MOVESPEED
+		
